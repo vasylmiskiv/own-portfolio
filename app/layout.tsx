051b6c07a14f6bdbd93205a34a310f5edce8e74c
vasylmiskiv <vasylmiskiv@gmail.com>
2023-06-22 11:46:55 +0300

    seach console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Vasyl Miskiv",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
